81aa548ef758e1470759e18811365c56dc17eda4
Mohanwebdev <thehider2001@gmail.com>
2023-03-22 13:55:25 +0530

    styles updated

--- src/components/ForecastDetails.js
+++ src/components/ForecastDetails.js
@@ -3,7 +3,7 @@ import React from "react";
 function ForecastDetails(props){
     return ( <div className="forecastDetails row">
     <div className="col-3"><span>{props.day}</span></div>
-    <div className="col-6"><img src={props.img}/>
+    <div className="col-6 desc"><img src={props.img}/>
        {props.desc}</div>
         
         <div className="col-3"> <span>{Math.round(props.min)}°C / {Math.round(props.max)}°C</span></div>
