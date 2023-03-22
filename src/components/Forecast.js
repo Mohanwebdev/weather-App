0e2d782bc7ae62216e4d8b59237f81686ab883e5
Mohanwebdev <thehider2001@gmail.com>
2023-03-22 18:28:53 +0530

    final commit

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,14 +4,17 @@ import ForecastDetails from "./ForecastDetails";
 
 function Forecast(props) {
 
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var date = new Date();
-    var day = date.getDay();
-    const dayInWeek = days.splice(days[day],days.length).concat(days.splice(days[0],days[day]));
+    // const dayInWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    const dayInWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    const date = new Date();
+    const day = date.getDay();
+    const newDayInWeek = dayInWeek.slice(day,dayInWeek.length).concat(dayInWeek.slice(0,day));
+
+
 
    return <div className="daily container text-center">
    {props.forecast.list.splice(0,7).map((item,idx)=>{
-    return <ForecastDetails key={idx} day={dayInWeek[idx]} img={`./icons/${item.weather[0].icon}.png`} desc={item.weather[0].description} min={item.main.temp_min} max={item.main.temp_max}/>
+    return <ForecastDetails key={idx} day={newDayInWeek[idx]} img={`./icons/${item.weather[0].icon}.png`} desc={item.weather[0].description} min={item.main.temp_min} max={item.main.temp_max}/>
    })}
    </div>
 }
